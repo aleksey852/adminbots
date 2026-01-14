a780bf372b36ae3f438f68142a2f1a7e71be41d0
aleksey852 <aaforworkonly@gmail.com>
2026-01-14 21:34:12 +0300

    4

--- admin_panel/routers/__init__.py
+++ admin_panel/routers/__init__.py
@@ -4,5 +4,11 @@ from .bots import router as bots_router
 from .users import router as users_router
 from .campaigns import router as campaigns_router
 from .system import router as system_router
+
+__all__ = [
+    'auth_router',
+    'system_router',
+    'users_router',
+    'bots_router',
     'campaigns_router',
 ]
